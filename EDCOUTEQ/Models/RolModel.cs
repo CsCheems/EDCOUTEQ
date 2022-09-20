847e4d96c332cc34a1b22ea957d859357aae52b2
JR <JR@DESKTOP-ADDCRJO>
2022-09-19 23:24:25 -0500

    Creacion de modelo Rol

---
+++ EDCOUTEQ/Models/RolModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDCOUTEQ.Models
+{
+    public class RolModel
+    {
+
+        public int id { get; set; }
+
+        public string rolUser { get; set; }
+
+
+
+    }
+}
